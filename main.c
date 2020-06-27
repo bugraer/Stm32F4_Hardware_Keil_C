2377a6e06b4751598496a932729b1f865c10439a
Buğra ER <67065146+bugraer@users.noreply.github.com>
2020-06-27 09:41:32 +0300

    microcontroller_to_computer

--- main.c
+++ main.c
@@ -1,56 +1,33 @@
 #include "stm32f4xx.h"
 
-int frekans=40000;
-int ho=250;
-int lo=7750;
-int deger=100;
+int i,j;
+char data[15]="abcdefghi\n";
+// mikro denetleyiciden bilgisayara veri gönderme 
+int main(void){	
 
-void SysTick_Handler(){
-	
-	if(ho>79500){
-		deger= deger*-2;
-		ho=79350;
+
+	RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+	GPIOA->MODER |= GPIO_MODER_MODE2_1; //Tx PA2
+	//Alternatif fonksiyon ayari , modera 10 yazilmasi Rx PA3
+	//GPIOA->MODER |= GPIO_MODER_MODE3_1;
+	//speed register, High speed
+	//GPIOA->OSPEEDR |=GPIO_OSPEEDR_OSPEED3_1;
+	//GPIOA->OSPEEDR |=GPIO_OSPEEDR_OSPEED2_1;
+	//USART1 Altenatif fonksiyon 7 olarak tanimli
+	//GPIOA->AFR[0] |= GPIO_AFRL_AFSEL3_2 | GPIO_AFRL_AFSEL3_1 | GPIO_AFRL_AFSEL3_0;
+	GPIOA->AFR[0] |= GPIO_AFRL_AFSEL2_2 | GPIO_AFRL_AFSEL2_1 | GPIO_AFRL_AFSEL2_0;
+
+	USART2->BRR =  0x683U; //9600
+	USART2->CR1 |= USART_CR1_UE;
+	USART2->CR1 |= USART_CR1_TE;
+
+	while(1){		
+		for(i=0;i<=10;i++){
+			USART2->DR=data[i];
+			while(!(USART2->SR & USART_SR_TXE));
+			while(!(USART2->SR & USART_SR_TC));
+		}
+		for(j=0;j<1000000;j++);					
 	}
-	if(ho<500){
-		deger= deger*-2;
-		ho=650;
-	}
-	
-	//asagidaki sekilde tek bir if icerisinde de yapilabilir.
-	//if(duty_cycle > 80000-500 || duty_cycle < 500){
-	//	deger = -1*deger;
-	//	duty_cycle+=deger;
-	// KADEMELI OLARAK LED YANIP SÖNER	}
-	
-	if(GPIOD->ODR & GPIO_ODR_OD14){
-		
-		ho=ho+deger;
-		lo=frekans-ho;
-		
-		GPIOD->ODR &= ~GPIO_ODR_OD14;
-		SysTick->LOAD &= ~SysTick_LOAD_RELOAD_Msk;
-		SysTick->LOAD |= ho & SysTick_LOAD_RELOAD_Msk;
-	}
-	else{
-		GPIOD->ODR |= GPIO_ODR_OD14;
-		SysTick->LOAD &= ~SysTick_LOAD_RELOAD_Msk;
-		SysTick->LOAD |= lo & SysTick_LOAD_RELOAD_Msk;
-	}
-}
-int main(){
-	SysTick->LOAD &= ~SysTick_LOAD_RELOAD_Msk;
-	SysTick->LOAD |= ho & SysTick_LOAD_RELOAD_Msk;
-	
-	SysTick->CTRL |= SysTick_CTRL_CLKSOURCE_Msk;
-	SysTick->CTRL |= SysTick_CTRL_TICKINT_Msk;
-	
-	NVIC_EnableIRQ(SysTick_IRQn);
-	
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
-	GPIOD->MODER |= GPIO_MODER_MODER14_0;
-	
-	SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;
-	GPIOD->ODR |= GPIO_ODR_OD14;
-	
-	while(1);	
 }
